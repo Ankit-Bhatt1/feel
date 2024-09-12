c10fe978165c5d4b6ce7af5bc35fbacddb7d353f
Ankit-Bhatt1 <bhattankit515@gmail.com>
2024-09-13 03:53:49 +0530

    Fixed Error on "you're"

--- components/Login.jsx
+++ components/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
   return (
     <div className='flex flex-col flex-1 justify-center items-center gap-4 '>
       <h3 className={'text-4xl '+ fugaz.className}>{isRegitser ? 'Register':'Log In'}</h3>
-      <p>You're one step away !</p>
+      <p>You&apos;re one step away !</p>
       <input value={email} onChange={(e)=>{setEmail(e.target.value)}} className='w-full max-w-[400px] px-4 py-2 border border-solid hover:border-indigo-500 rounded-full focus:border-indigo-600 border-indigo-300' placeholder='Email'/>
       <input value={password} onChange={(e)=>{setPassword(e.target.value)}} className='w-full max-w-[400px] px-4 py-2 border border-solid hover:border-indigo-500 rounded-full focus:border-indigo-600 border-indigo-300' placeholder='Password' type='password'/>
       <div>
